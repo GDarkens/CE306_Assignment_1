c9f11f1f9db6a54361d9dcf4c6289bdef65444c8
George <gd17659@essex.ac.uk>
2020-02-19 17:55:39 +0000

    Conversion to non-static, multi-document handling ability
    Reading about TF*IDF being required confused me since we are not given a set corpus to work on and TF*IDF doesn't make sense on a single document.
    However it turns out the two given sites are the entire corpus, so I made the program work to create an object, PipelineObject, that contains all the data I have found or created for each document and compartmentalised it in an index-able object.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,10 @@
 package com.company;
 
 import edu.stanford.nlp.ling.TaggedWord;
-import org.jsoup.nodes.Document;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static com.company.DocumentProcessing.*;
-import static com.company.TextProcessing.*;
+import java.util.*;
 
 public class Main {
-    static Document webpageDoc;
-    static String webpageText;
-
-
-
     static ArrayList<String> webpageBulletArray;
     static ArrayList<String> webpageTableArray;
     static ArrayList<String> webpageSentenceArray;
@@ -27,6 +15,9 @@ public class Main {
     static List<TaggedWord> taggedTableList;
     static List<TaggedWord> taggedMetatagList;
 
+    static List<String> CORPUS_URLS = new LinkedList<>();
+
+
     public static void main(String[] args) {
         webpageSentenceArray = new ArrayList<>();
         webpageMetatagArray = new ArrayList<>();
@@ -34,44 +25,22 @@ public class Main {
         webpageTableArray = new ArrayList<>();
         Set<String> keywordSet = new HashSet<>();
 
-        //http://www.multimediaeval.org/mediaeval2019/memorability/
-        //https://sites.google.com/view/siirh2020/
+        CORPUS_URLS.add("http://www.multimediaeval.org/mediaeval2019/memorability/");
+        CORPUS_URLS.add("https://sites.google.com/view/siirh2020/");
 
 
-        webpageDoc = webpageGrabber("http://www.multimediaeval.org/mediaeval2019/memorability/");
-        tablesAndBulletExtract(webpageDoc);
-        metaKeywordExtract(webpageDoc);
-
-        webpageText = htmlParse(webpageDoc);
-
-        textPreProcessor(webpageText);
-
-        //Just calls partOfSpeechTagging() on all data I have off the webpage
-        tagHandler();
-
-        //Calls nounPhraseChunkHandler() tagged data from tagHandler()
-        keywordSet.addAll(nounPhraseChunkHandler());
+        List<PipelineObject> documentList = new LinkedList<>();
+        for(String URL : CORPUS_URLS){
+            documentList.add(new PipelineObject(URL));
+        }
 
 
 
+        for(PipelineObject doc : documentList){
+            System.out.println(doc.theUrl);
+        }
 
-    }
-
-    static Set<String> nounPhraseChunkHandler(){
-        Set<String> phraseSet = new HashSet<>();
-        phraseSet.addAll(nounPhraseChunkExtract(taggedSentenceList));
-        phraseSet.addAll(nounPhraseChunkExtract(taggedBulletList));
-        phraseSet.addAll(nounPhraseChunkExtract(taggedMetatagList));
-        phraseSet.addAll(nounPhraseChunkExtract(taggedTableList));
-
-
-        return phraseSet;
-    }
-
-    static void tagHandler(){
-        taggedSentenceList = partOfSpeechTagging(webpageSentenceArray);
-        taggedBulletList = partOfSpeechTagging(webpageBulletArray);
-        taggedMetatagList = partOfSpeechTagging(webpageMetatagArray);
-        taggedTableList = partOfSpeechTagging(webpageTableArray);
     }
 }
+
+
---
+++ src/com/company/PipelineObject.java
@@ -0,0 +1,79 @@
+package com.company;
+
+import edu.stanford.nlp.ling.TaggedWord;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.company.DocumentProcessing.*;
+import static com.company.TextProcessing.*;
+
+class PipelineObject {
+    Document webpageDoc;
+    String webpageText;
+
+    ArrayList<String> webpageBulletArray;
+    ArrayList<String> webpageTableArray;
+    ArrayList<String> webpageSentenceArray;
+    ArrayList<String> webpageMetatagArray;
+
+    List<TaggedWord> taggedSentenceList;
+    List<TaggedWord> taggedBulletList;
+    List<TaggedWord> taggedTableList;
+    List<TaggedWord> taggedMetatagList;
+
+    String theUrl;
+
+    PipelineObject(String URL){
+        webpageSentenceArray = new ArrayList<>();
+        webpageMetatagArray = new ArrayList<>();
+        webpageBulletArray = new ArrayList<>();
+        webpageTableArray = new ArrayList<>();
+        Set<String> keywordSet = new HashSet<>();
+
+        theUrl = URL;
+
+
+        webpageDoc = webpageGrabber(URL);
+
+        tablesAndBulletExtract(webpageDoc);
+        metaKeywordExtract(webpageDoc);
+
+        webpageText = htmlParse(webpageDoc);
+
+        textPreProcessor(webpageText);
+
+        //Just calls partOfSpeechTagging() on all data I have off the webpage
+        tagHandler();
+
+        //Calls nounPhraseChunkHandler() tagged data from tagHandler()
+        keywordSet.addAll(nounPhraseChunkHandler());
+
+        //tfIdf(webpageSentenceArray, "and");
+
+    }
+
+    Set<String> nounPhraseChunkHandler(){
+        Set<String> phraseSet = new HashSet<>();
+        phraseSet.addAll(nounPhraseChunkExtract(taggedSentenceList));
+        phraseSet.addAll(nounPhraseChunkExtract(taggedBulletList));
+        phraseSet.addAll(nounPhraseChunkExtract(taggedMetatagList));
+        phraseSet.addAll(nounPhraseChunkExtract(taggedTableList));
+
+
+        return phraseSet;
+    }
+
+    void tagHandler(){
+        taggedSentenceList = partOfSpeechTagging(webpageSentenceArray);
+        taggedBulletList = partOfSpeechTagging(webpageBulletArray);
+        taggedMetatagList = partOfSpeechTagging(webpageMetatagArray);
+        taggedTableList = partOfSpeechTagging(webpageTableArray);
+    }
+
+}
+
+
